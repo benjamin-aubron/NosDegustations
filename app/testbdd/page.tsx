7f2f294127cdbab4a2c36fc7acae54908221ff68
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-09-17 19:26:21 +0200

    neon & prisma setup

---
+++ app/testbdd/page.tsx
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client"
+import addCommentToDB from "./addComment"
+
+const prisma = new PrismaClient()
+
+export default async function page() {
+
+  const data = await prisma.comment.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    select: {
+      id: true,
+      comment: true,
+    },
+  })
+
+  async function addComment(formData: FormData) {
+    'use server'
+    const id = formData.get('id') as string
+    const comment = formData.get('comment') as string
+    await prisma.comment.create({
+      data: {
+        id,
+        comment,
+      },
+    })
+  }
+
+
+  return (
+    <div>
+      <h1 className="text-4xl font-medium mb-4">Test BDD</h1>
+      <div>{data.map((d) => <p key={d.id}>{d.comment}</p>)}</div>
+      <form action={addComment} className="flex flex-col my-10 justify-start space-y-4">
+        <input name="id" type="text" placeholder="Entrer l'ID" className="bg-neutral-200 w-50 rounded px-2 py-1"/>
+        <input name="comment" type="text" placeholder="Entrer le commentaire" className="bg-neutral-200 w-50 rounded px-2 py-1"/>
+        <button className="bg-neutral-300 hover:bg-neutral-400 rounded px-2 py-1 w-50" type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
