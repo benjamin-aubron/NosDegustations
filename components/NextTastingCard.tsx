7690d2799710cd3e78e5c164a2a619749d0f8be1
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-09-19 11:54:54 +0200

    deletion no longer fills inputs

--- components/NextTastingCard.tsx
+++ components/NextTastingCard.tsx
@@ -9,21 +9,23 @@ import deleteNextTasting from "@/app/prochaines-degustations/deleteTasting";
 
 type NextTastingCardProps = Pick<Data, "id" | "appelation" | "region">
 
-export default function NextTastingCard({id, appelation, region, onClick }: NextTastingCardProps & { onClick?: () => void }) {
+export default function NextTastingCard({ id, appelation, region, onClick }: NextTastingCardProps & { onClick?: () => void }) {
 
   const pathname = usePathname()
 
   return (
-    <Card className="my-2 p-4 rounded-lg cursor-pointer" onClick={onClick}>
-      <div className="flex justify-between items-center">
-        <div className="">
-          <div className="text-base font-semibold">{appelation}</div>
-          <div className="text-sm text-neutral-600">{region}</div>
+    <div className="relative"> 
+      <Card className="my-2 p-4 rounded-lg cursor-pointer" onClick={onClick}>
+        <div className="flex justify-between items-center">
+          <div className="">
+            <div className="text-base font-semibold">{appelation}</div>
+            <div className="text-sm text-neutral-600">{region}</div>
+          </div>
         </div>
-        <div onClick={() => {deleteNextTasting(id)}} className={`${pathname === "/" ? "hidden" : "flex"} flex-col justify-center items-end text-sm text-neutral-600 bg-red-50 hover:bg-red-200 p-2 cursor-pointer rounded-sm group`}>
-          <Trash2 className="w-6 h-6 text-neutral-500 group-hover:text-red-900" />
-        </div>
+      </Card>
+      <div onClick={() => { deleteNextTasting(id) }} className={`${pathname === "/" ? "hidden" : "flex"} flex-col justify-center items-end text-sm text-neutral-600 bg-red-50 hover:bg-red-200 p-2 cursor-pointer rounded-sm group absolute top-1/2 -translate-y-1/2 right-4`}>
+        <Trash2 className="w-6 h-6 text-neutral-500 group-hover:text-red-900" />
       </div>
-    </Card>
+    </div>
   )
 }
