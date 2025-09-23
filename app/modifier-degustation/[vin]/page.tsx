ecf31805355de78ee06e4d671a6e0fff55a74606
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-09-23 10:42:54 +0200

    feat - delete button for modify vin

--- app/modifier-degustation/[vin]/page.tsx
+++ app/modifier-degustation/[vin]/page.tsx
@@ -1,6 +1,9 @@
 import TestedForm from "@/components/TestedForm"
 import fetchSelected from "@/app/ajouter-degustations/fetchSelected"
 import NotFound from "@/app/not-found"
+import Link from "next/link"
+import { ArrowLeft } from "lucide-react"
+import { Trash2 } from "lucide-react"
 
 type StringifiedData = {
   appelation: string;
@@ -37,6 +40,12 @@ export default async function Page({ params }: { params: Promise<{ vin: string }
 
   return (
     <div>
+      <div className="flex justify-between">
+        <Link href={`/`} className="flex items-center gap-1 text-lg hover:bg-neutral-200 rounded-lg pl-2 pr-3 py-2"> <ArrowLeft className="w-5 h-5" /> Retour</Link>
+        <div className="flex-col cursor-pointer justify-center group items-end text-sm bg-neutral-200 hover:bg-red-200 p-3 rounded-lg text-neutral-700 hover:text-red-900">
+          <Trash2 className="w-6 h-6" />
+        </div>
+      </div>
       <h1 className="text-4xl font-medium my-8">Modifier {selectedWine?.appelation}</h1>
       <TestedForm DefaultValues={formattedSelectedWine} />
     </div>
