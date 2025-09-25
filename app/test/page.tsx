656553311cb9186476fab65609de413e85a279b0
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-09-25 11:29:11 +0200

    test uploadcare

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+import ImageUploader from "@/components/ImageUploader"
+
+export default function page() {
+  return (
+    <div>
+      <ImageUploader />
+      <img src="https://1hlc84ue55.ucarecd.net/06c2a117-550c-4b91-ae5f-5d2d8fe8e6d9/-/preview/1500x1146/"/>
+    </div>
+  )
+}
